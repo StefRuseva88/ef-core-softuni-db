85b599b9629fbebb23d67333cb218975d721e07f
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-24 12:12:58 +0300

    Add files via upload

---
+++ Entity_Framework/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            Console.WriteLine("Database created successfully!");
+        }
+    }
+
+}
